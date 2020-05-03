8654dbb362f6bb227d21de6671c73ffc240331f7
Sogyal <sogyals429@gmail.com>
2020-05-03 13:32:24 +1000

    Initial Commit

---
+++ 01-driver.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-ecs-sogyal"
+    key = "terraform.tfstate.d"
+    region = "ap-southeast-2"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region = "ap-southeast-2"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
+data "external" "git_branch" {
+  program = [ "scripts/get_branch.sh" ]
+}
+
+locals {
+  common_tags = {
+    Project = "terraform-aws-vpn"
+    Maintainer_Software = "Terraform"
+    Revision = "${data.external.git_branch.result["output"]}"
+    Project = "git@github.com:sogyals429/terraform-aws-vpn.git"
+  }
+}
