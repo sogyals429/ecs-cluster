8b6f9d7efee6d594f14e7acce3eb4720238e8010
Sogyal Sherpa <sogyals429@gmail.com>
2020-09-20 20:56:59 +1000

    Basic Network Setup

--- 02-network.tf
+++ 02-network.tf
@@ -26,14 +26,14 @@ resource "aws_route_table" "ecs-rt" {
 
 resource "aws_route_table_association" "ecs-rt-assoc" {
   count          = length(aws_subnet.public)
-  route_table_id = element(aws_route_table.ecs-rt.*.id,count.index)
+  route_table_id = element(aws_route_table.ecs-rt.*.id, count.index)
   subnet_id      = element(aws_subnet.public.*.id, count.index)
 }
 
 resource "aws_route" "public" {
   count                  = length(aws_subnet.public)
   destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = element(aws_route_table.ecs-rt.*.id,count.index)
+  route_table_id         = element(aws_route_table.ecs-rt.*.id, count.index)
   gateway_id             = aws_internet_gateway.igw.id
 }
 
@@ -62,13 +62,13 @@ resource "aws_route_table" "private-rt" {
 
 resource "aws_route_table_association" "ecs-rt-assoc" {
   count          = length(aws_subnet.private)
-  route_table_id = element(aws_route_table.private-rt.*.id,count.index)
+  route_table_id = element(aws_route_table.private-rt.*.id, count.index)
   subnet_id      = element(aws_subnet.private.*.id, count.index)
 }
 
-resource "aws_route" "private_rt"{
-  count                   = length(aws_subnet.private)
-  destination_cidr_block  = "0.0.0.0/0"
-  route_table_id = element(aws_route_table.private-rt.*.id,count.index)
-  nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
+resource "aws_route" "private_rt" {
+  count                  = length(aws_subnet.private)
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = element(aws_route_table.private-rt.*.id, count.index)
+  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
 }
