bdffb0e807deffad5102bd4514b29a493803b407
Sogyal Sherpa <sogyals429@gmail.com>
2020-09-20 21:09:24 +1000

    Basic Network Setup

--- 02-network.tf
+++ 02-network.tf
@@ -3,19 +3,19 @@ resource "aws_vpc" "main" {
   tags       = merge({ Name = "ecs-vpc" }, local.common_tags)
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = merge({ Name = "ecs-igw" }, local.common_tags)
+}
+
 # Public Network
 
 resource "aws_subnet" "public" {
   count                   = length(data.aws_availability_zones.available.names)
   cidr_block              = element(var.public_subnets, count.index)
-  vpc_id                  = aws_vpc.ecs-vpc.id
+  vpc_id                  = aws_vpc.main.id
   map_public_ip_on_launch = true
   tags                    = merge({ Name = "ecs-pub-subnet-${count.index}" }, local.common_tags)
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.ecs-vpc.id
-  tags   = merge({ Name = "ecs-igw" }, local.common_tags)
 }
 
 resource "aws_route_table" "ecs-rt" {
@@ -46,9 +46,16 @@ resource "aws_subnet" "private" {
   tags       = merge({ Name = "ecs-priv-subnet-${count.index}" }, local.common_tags)
 }
 
+resource "aws_eip" "ngw-ip" {
+  depends_on = [aws_internet_gateway.igw]
+  count    = length(aws_subnet.public)
+  vpc      = true
+  associate_with_private_ip = element(var.private_subnets,count.index)
+}
+
 resource "aws_nat_gateway" "ngw" {
   count         = length(aws_subnet.public)
-  allocation_id = aws_eip.nat.id
+  allocation_id = element(aws_eip.ngw-ip.*.id, count.index)
   subnet_id     = element(aws_subnet.public.*.id, count.index)
   tags          = merge({ Name = "nat-gw-${count.index}" }, local.common_tags)
 }
@@ -60,7 +67,7 @@ resource "aws_route_table" "private-rt" {
   tags   = merge({ Name = "ecs-private-rt-${count.index}" }, local.common_tags)
 }
 
-resource "aws_route_table_association" "ecs-rt-assoc" {
+resource "aws_route_table_association" "priavte-ecs-rt-assoc" {
   count          = length(aws_subnet.private)
   route_table_id = element(aws_route_table.private-rt.*.id, count.index)
   subnet_id      = element(aws_subnet.private.*.id, count.index)
--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 ecs:
 	terraform apply --var-file variable.tfvars
 
+plan.ecs:
+	terraform plan --var-file variable.tfvars
+
 destroy:
 	terraform destroy --var-file variable.tfvars
