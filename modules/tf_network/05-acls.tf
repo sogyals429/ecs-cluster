f3caede140b3d5a443f2841e96dc7f602e8c3c9d
Sogyal Sherpa <sogyals429@gmail.com>
2020-09-21 17:40:24 +1000

    Creating public acls

--- modules/tf_network/05-acls.tf
+++ modules/tf_network/05-acls.tf
@@ -5,13 +5,53 @@ resource "aws_network_acl" "public" {
   subnet_ids = aws_subnet.public.*.id
 }
 
+resource "aws_network_acl_rule" "https_inbound"{
+  network_acl_id = aws_network_acl.public.*.id
+  rule_number    = 100
+  egress         = false
+  protocol       = "https"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "https_outbound"{
+  network_acl_id = aws_network_acl.public.*.id
+  rule_number    = 101
+  egress         = true
+  protocol       = "https"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 443
+  to_port        = 443
+}
+
+resource "aws_network_acl_rule" "ssh_inbound"{
+  network_acl_id = aws_network_acl.public.*.id
+  rule_number    = 102
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "103.44.33.69/32"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "ssh_outbound"{
+  network_acl_id = aws_network_acl.public.*.id
+  rule_number    = 103
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 22
+  to_port        = 22
+}
+
 # Private Network
 
 resource "aws_network_acl" "private" {
   vpc_id = aws_vpc.main.id
   subnet_ids = aws_subnet.private.*.id
 }
-
-# resource "aws_network_acl_rule" "https"{
-  
-# }
