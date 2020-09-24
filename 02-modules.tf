7cd479f924c063d38b7bd8bab968a436380dc0b2
Sogyal Sherpa <sogyals429@gmail.com>
2020-09-24 17:34:00 +1000

    Formatting

--- 02-modules.tf
+++ 02-modules.tf
@@ -1,11 +1,11 @@
 module "tf_ecs" {
-  source                  = "./modules/tf_ecs"
-  private_subnets         = var.private_subnets
-  ecs_config_bucket_arn   = module.tf_s3.terraform_ecs_config_bucket_arn
-  ecs_config_bucket       = module.tf_s3.terraform_ecs_config_bucket_name
-  ecs_log_group           = module.tf_cloudwatch.ecs_cloudwatch_log_group
-  aws_region              = var.aws_region
-  common_tags             = local.common_tags
+  source                = "./modules/tf_ecs"
+  private_subnets       = var.private_subnets
+  ecs_config_bucket_arn = module.tf_s3.terraform_ecs_config_bucket_arn
+  ecs_config_bucket     = module.tf_s3.terraform_ecs_config_bucket_name
+  ecs_log_group         = module.tf_cloudwatch.ecs_cloudwatch_log_group
+  aws_region            = var.aws_region
+  common_tags           = local.common_tags
 }
 
 module "tf_network" {
