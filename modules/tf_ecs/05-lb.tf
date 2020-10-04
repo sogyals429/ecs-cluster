44a65aecff7bdfce8115284d8349effe86690081
Sogyal Sherpa <sogyals429@gmail.com>
2020-10-04 12:44:56 +1100

    Test

--- modules/tf_ecs/05-lb.tf
+++ modules/tf_ecs/05-lb.tf
@@ -34,7 +34,7 @@ resource "aws_lb_listener" "api_lb_listener" {
 
 resource "aws_lb_target_group" "api_lb_target_group" {
   name              = "api-lb-proxy-tg"
-  port              = 80
+  port              = 443
   protocol          = "TCP"
   target_type       = "ip"
   vpc_id            = var.vpc_id
